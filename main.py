14723304142bbafce3af57e8709b0b3eb647620d
ManthanMarathe223 <manthanmarathe223@gmail.com>
2025-10-19 19:54:12 +0530

    fix: Restrict CORS origins to specific domains for enhanced security

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ app = FastAPI(title="Crop Recommendation API", version="2.0")
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://crop-recommendation-frontend-gamma.vercel.app", 
+                   "http://localhost:3000",
+                   "http://127.0.0.1:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
